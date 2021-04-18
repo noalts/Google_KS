fc226ede26525d2f05f44b6aa53895768f308235
noalts <noalts@web.de>
2021-04-18 14:40:41 +0200

    new

--- snippets.py
+++ snippets.py
@@ -841,3 +841,31 @@ for i in range(n):
     a[i][n - i - 1] = '*'
 for row in a:
     print(' '.join(row))
+
+#print chessboard:
+n, m = [int(i) for i in input().split()]
+a = [['.'] * m for i in range(n)]
+
+for i in range(n):
+    if i % 2 != 0:
+        for j in range(0, m, 2):        
+            a[i][j] = "*"
+    else:
+        for j in range(1, m, 2):
+            a[i][j] = "*"
+
+for row in a:
+    print(' '.join(row))
+
+# or:
+n, m = [int(i) for i in input().split()]
+a = []
+for i in range(n):
+    a.append([])
+    for j in range(m):
+        if (i + j) % 2 == 0:
+            a[i].append('.')
+        else:
+            a[i].append('*')
+for row in a:
+    print(' '.join(row))
--- tests.py
+++ tests.py
@@ -1,9 +1,14 @@
-n = int(input())
-a = [['.'] * n for i in range(n)]
+n, m = [int(i) for i in input().split()]
+a = [['.'] * m for i in range(n)]
+
 for i in range(n):
-    a[i][i] = '*'
-    a[n // 2][i] = '*'
-    a[i][n // 2] = '*'
-    a[i][n - i - 1] = '*'
+    if i % 2 != 0:
+        for j in range(0, m, 2):        
+            a[i][j] = "*"
+    else:
+        for j in range(1, m, 2):
+            a[i][j] = "*"
+
 for row in a:
     print(' '.join(row))
+
