43abf0948502b254a120001644464d9e1bc5a97e
noalts <noalts@web.de>
2021-04-20 23:07:46 +0200

    new

--- snippets.py
+++ snippets.py
@@ -869,3 +869,25 @@ for i in range(n):
             a[i].append('*')
 for row in a:
     print(' '.join(row))
+
+#parallel diagonals readable:
+n = int(input())
+a = [['0'] * n for i in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if i < j:
+            a[i][j] = str(j-i)
+        elif i == j:
+            a[i][j] = str(0)
+        else:
+            a[i][j] = str(i-j)
+
+for row in a:
+    print(' '.join(row))
+
+#or pythonic
+n = int(input())
+a = [[abs(i - j) for j in range(n)] for i in range(n)]
+for row in a:
+    print(' '.join([str(i) for i in row]))
--- tests.py
+++ tests.py
@@ -1,13 +1,14 @@
-n, m = [int(i) for i in input().split()]
-a = [['.'] * m for i in range(n)]
+n = int(input())
+a = [['0'] * n for i in range(n)]
 
 for i in range(n):
-    if i % 2 != 0:
-        for j in range(0, m, 2):        
-            a[i][j] = "*"
-    else:
-        for j in range(1, m, 2):
-            a[i][j] = "*"
+    for j in range(n):
+        if i < j:
+            a[i][j] = str(j-i)
+        elif i == j:
+            a[i][j] = str(0)
+        else:
+            a[i][j] = str(i-j)
 
 for row in a:
     print(' '.join(row))
