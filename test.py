3ccd57e4333dbdbac4e98dfeb85fcf3e0bf5b50f
noalts <noalts@web.de>
2021-04-23 15:12:26 +0200

    new

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-# input() reads a string with a line of input, stripping the ' ' (newline) at the end.
-# This is all you need for most problems.
-t = int(input()) # read a line with a single integer
-for i in range(1, t + 1):
-    n, m = [int(s) for s in input().split(" ")] # read a list of integers, 2 in this case
-    print("Case #{}: {} {}".format(i, n + m, n * m))
-    # check out .format's specification for more formatting options
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[i][j] = X[j][i]
+
+for r in result:
+   print(r)
--- tests.py
+++ tests.py
@@ -1,16 +1,30 @@
 
-def scale(a, n, m, c):    
-    for i in range(n):
-        for j in range(m):
-            a[i][j] *= c
+def multi(a, b, c, d, m, n, r):
+        
+    for i in range(m):
+        q = 0
+        for j in range(n):
+            q += a[i][j] * c[i][j]
+        d.append(q)
+
+
+       
     
 
-n, m = [int(i) for i in input().split()]
-a = [[int(j) for j in input().split()] for i in range(n)]
-c = int(input())
+m, n, r = [int(i) for i in input().split()]
+a = [[int(j) for j in input().split()] for i in range(m)]
+b = [[int(k) for k in input().split()] for l in range(n)]
+d = []
 
-scale(a, n, m, c)
+c = [[b[j][i] for j in range(len(b))] for i in range(len(b[0]))]
 
-for row in a:
+# for i in range(len(b)):
+#     for j in range(len(b[0])):
+#         c[i][j] = b[j][i]
+
+
+multi(a, b, c, d, m, n, r)
+
+for row in c:
     print(' '.join([str(elem) for elem in row]))
 
