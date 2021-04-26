1e2089ac218485a78968b166c8973f0fbcf9b41f
noalts <noalts@web.de>
2021-04-26 23:41:29 +0200

    new

--- snippets.py
+++ snippets.py
@@ -959,4 +959,35 @@ for i in range(m):
     for j in range(n):
         A[i][j] *= c
 
-print('\n'.join([' '.join([str(k) for k in row]) for row in A]))    
+print('\n'.join([' '.join([str(k) for k in row]) for row in A]))
+
+#multiply different arrays with oneonother
+def multi(a, b, d):
+        
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                d[i][j] += a[i][k] * b[k][j]   
+
+m, n, r = [int(i) for i in input().split()]
+a = [[int(j) for j in input().split()] for i in range(m)]
+b = [[int(k) for k in input().split()] for l in range(n)]
+d = [[int(0) for s in range(r)] for t in range(m)]
+
+multi(a, b, d)
+
+for row in d:
+    print(' '.join([str(elem) for elem in row]))
+
+#or without function:
+m, n, r = [int(k) for k in input().split()]
+A = [[int(k) for k in input().split()] for i in range(m)]
+B = [[int(k) for k in input().split()] for j in range(n)]
+C = [[0]*r for i in range(m)]
+
+for i in range(m):
+    for k in range(r):
+        for j in range(n):
+            C[i][k] += A[i][j] * B[j][k]
+
+print('\n'.join([' '.join([str(k) for k in row]) for row in C]))
