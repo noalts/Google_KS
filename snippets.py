3b00866c22f0e81f8f186062303305a00cb1f474
noalts <noalts@web.de>
2021-05-24 18:28:56 +0200

    new

--- snippets.py
+++ snippets.py
@@ -1185,3 +1185,81 @@ for i in range(int(input())):
 max_count = max(counter.values())
 most_frequent = [k for k, v in counter.items() if v == max_count]
 print(min(most_frequent))
+
+# Permissions
+s = dict()
+b = dict()
+n = int(input())
+max = 0
+nmax = 0
+tfile = []
+a = []
+fn = str()
+key = ''
+val = ''
+
+for i in range(n):
+    tfile.append(input().split())
+
+def ltod(x, dict):
+    val = []
+    for k in x:
+        for j in range(len(k)):
+            if j == 0:
+                key = k[j]
+                dict[key] = key
+            else:
+                val.append(k[j])
+        dict[key] = ''.join(val)
+        val =[]
+    return dict
+
+ltod(tfile, s)
+
+
+l = int(input())
+files = []
+for i in range(l):
+    r = input().split()
+    r.reverse()
+    files.append(r)
+
+
+for i in files:
+    for j in range(len(i)):
+        if i[j] == 'write':
+            i[j] = 'W'
+        elif i[j] == 'execute':
+            i[j] = 'X'
+        elif i[j] == 'read':
+            i[j] = 'R'
+
+for i in range(len(files)):
+    key, val = files[i][0], files[i][1]
+    
+    if key in s:
+        
+        if val in s.get(key):
+            print('OK')
+        else:
+            print('Access denied')
+
+# or pythonic:
+
+OPERATION_PERMISSION = {
+    'read': 'R',
+    'write': 'W',
+    'execute': 'X',
+}
+
+file_permissions = {}
+for i in range(int(input())):
+    file, *permissions = input().split()
+    file_permissions[file] = set(permissions)
+
+for i in range(int(input())):
+    operation, file = input().split()
+    if OPERATION_PERMISSION[operation] in file_permissions[file]:
+        print('OK')
+    else:
+        print('Access denied')
--- tests.py
+++ tests.py
@@ -3,33 +3,101 @@ b = dict()
 n = int(input())
 max = 0
 nmax = 0
+tfile = []
 a = []
+fn = str()
+key = ''
+val = ''
+
 for i in range(n):
-    words = input().split()
-    for w in words:
-        if w not in s:
-            key, val = w, 1
-            s[key] = val
-            
+    tfile.append(input().split())
+
+def ltod(x, dict):
+    val = []
+    for k in x:
+        for j in range(len(k)):
+            if j == 0:
+                key = k[j]
+                dict[key] = key
+            else:
+                val.append(k[j])
+        dict[key] = ''.join(val)
+        val =[]
+    return dict
+
+ltod(tfile, s)
+
+
+l = int(input())
+files = []
+for i in range(l):
+    r = input().split()
+    r.reverse()
+    files.append(r)
+
+
+for i in files:
+    for j in range(len(i)):
+        if i[j] == 'write':
+            i[j] = 'W'
+        elif i[j] == 'execute':
+            i[j] = 'X'
+        elif i[j] == 'read':
+            i[j] = 'R'
+
+for i in range(len(files)):
+    key, val = files[i][0], files[i][1]
+    
+    if key in s:
+        
+        if val in s.get(key):
+            print('OK')
         else:
-            key, val = w, s.get(key)
-            s[key] = s.get(key) + 1
+            print('Access denied')
 
 
-def filter(dictObj, callback):
-    nd = dict()
-    for (key, val) in dictObj.items():
-        if callback((key, val)):
-            nd[key] = val
-    return nd
+            # print(i[j])
+            # if i[j+1] in s[key]:
 
-r = sorted(s.items(), key=lambda i: (i[1]), reverse=True)
+            
+    
 
-max = list(r)[0][1]
-nd = filter(s, lambda e: int(e[1])>=max)
 
-nd = sorted(nd, key=lambda i: i[0])
-print(nd[0])
+
+# class file:
+#     def __init__(self, fi, read, write, ex):
+#         self.fi = fi
+#         self.read = read
+#         self.write = write
+#         self.ex = ex
+#     def __repr__(self):
+#         return repr((self.fi, self.read, self.write, self.ex))
+
+
+#     for w in file:
+#         if w not in s:
+#             key, val = w, 1
+#             s[key] = val
+            
+#         else:
+#             key, val = w, s.get(key)
+#             s[key] = s.get(key) + 1
+
+
+# def filter(dictObj, callback):
+#     nd = dict()
+#     for (key, val) in dictObj.items():
+#         if callback((key, val)):
+#             nd[key] = val
+#     return nd
+
+# r = sorted(s.items(), key=lambda i: (i[1]), reverse=True)
+
+# max = list(r)[0][1]
+# nd = filter(s, lambda e: int(e[1])>=max)
+
+# nd = sorted(nd, key=lambda i: i[0])
+# print(nd[0])
 # print(s)
 # print(s)
 # res =   
